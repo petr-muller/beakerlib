3e2a8377464ca55d51cf7e5d02573c439f4551ac
Petr Muller <muller@redhat.com>
2014-11-20 11:34:16 +0100

    First working interation of xUnit XSLT
    The transformation is build along the following rules:
    
    Single BL testcase translates to xUnit <testsuite> element, allowing
    aggregation of multiple BL test case runs into xUnit <testsuites>
    element.
    
    BeakerLib header records are translated into <testsuite> element
    attributes, where appropriate attribute exists (id, testname, hostname,
    package). The rest is translated to xUnit <property> element. The XSLT
    rules for these seems rather sloppy: perhaps there is a better way how
    to represent this rule (because these rules are almost identical except
    for the key), but I did not find it.
    
    BeakerLib phases translate to xUnit <testcase> elements, with a name
    mapping to the element attribute. This means the name does not need to
    be unique after translation. I do not know if this is okay in xUnit.
    
    xUnit records just FAILs, not PASSes. BL <test> elements are translated
    to either <failure> or <error> xUnit elements only if their content is
    FAIL. The phase type affects whether a <failure> or <error> is used. For
    FAIL (testing) phases, <failure> is used. For WARN (setup and cleanup)
    phases, <error> is used.

--- src/test/xunit/xunit-check.sh
+++ src/test/xunit/xunit-check.sh
@@ -29,14 +29,14 @@ xsltproc $XSLT $ORIGIN > $OUTPUT
 xmllint --noblanks $GOLDEN > $OUTPUT.golden.noblanks
 xmllint --noblanks $OUTPUT > $OUTPUT.noblanks
 
-if diff -q $OUTPUT.golden.noblanks $OUTPUT.noblanks
+if diff -qw $OUTPUT.golden.noblanks $OUTPUT.noblanks
 then
   echo "PASS: $ORIGIN"
   RESULT=0
 else
   xmllint --pretty 1 $GOLDEN > $OUTPUT.golden.pretty
   xmllint --pretty 1 $OUTPUT > $OUTPUT.pretty
-  diff -u $OUTPUT.golden.pretty $OUTPUT.pretty
+  diff -uw $OUTPUT.golden.pretty $OUTPUT.pretty
   echo "FAIL: $ORIGIN"
   RESULT=1
 fi
